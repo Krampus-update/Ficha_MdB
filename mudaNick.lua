f0f0df3c6972873ebd69ea38bc24cc1ce3f1641d
Krampus-update <pedro_br14@hotmail.com>
2023-03-06 01:16:57 -0300

    vai se foder samael

---
+++ mudaNick.lua
@@ -0,0 +1,16 @@
+function mudaNick(nome, sheet)
+    if not nome then return; end;
+    if not sheet then return; end;
+
+    if not common.isMainChar(sheet) then return; end;
+    if not common.isMyChar(sheet) then return; end;
+
+    local mesa = common.getMesa(sheet);
+    if not mesa then return; end;
+
+    if not mesa.meuJogador.isJogador then return; end;
+    if not (mesa.meuJogador.nick ~= nome) then return; end;
+
+    mesa.chat:enviarMensagem("/nick " .. nome);
+    mesa.chat:enviarMensagem("nick " .. nome);
+	end;
