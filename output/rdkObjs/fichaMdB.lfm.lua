1b404e27fcc06bcf8d9a0d56c144602d1356b09d
Zambrakas <gabrielvaladares323@gmail.com>
2023-03-08 02:20:43 +0000

    Mar 7, 2023, 11:20 PM

--- output/rdkObjs/fichaMdB.lfm.lua
+++ output/rdkObjs/fichaMdB.lfm.lua
@@ -4924,9 +4924,9 @@ require('mudaNick.lua')
 
     obj.flowPart52 = GUI.fromHandle(_obj_newObject("flowPart"));
     obj.flowPart52:setParent(obj.flowLayout12);
-    obj.flowPart52:setMinWidth(235);
-    obj.flowPart52:setMaxWidth(475);
-    obj.flowPart52:setMinScaledWidth(235);
+    obj.flowPart52:setMinWidth(370);
+    obj.flowPart52:setMaxWidth(470);
+    obj.flowPart52:setMinScaledWidth(370);
     obj.flowPart52:setHeight(250);
     obj.flowPart52:setMargins({left=2, right=2, top=4, bottom=8});
     obj.flowPart52:setName("flowPart52");
@@ -4934,8 +4934,8 @@ require('mudaNick.lua')
 
     obj.frame2 = GUI.fromHandle(_obj_newObject("frame"));
     obj.frame2:setParent(obj.flowPart52);
-    obj.frame2:setWidth(475);
-    obj.frame2:setHeight(211);
+    obj.frame2:setWidth(375);
+    obj.frame2:setHeight(200);
     obj.frame2:setFrameStyle("frames/panel4transp/frame.xml");
     obj.frame2:setMargins({left = 0, right = 4});
     obj.frame2:setName("frame2");
@@ -4966,7 +4966,7 @@ require('mudaNick.lua')
     obj.flowLayout15:setAutoHeight(true);
     obj.flowLayout15:setMinWidth(235);
     obj.flowLayout15:setMaxWidth(475);
-    obj.flowLayout15:setMinScaledWidth(105);
+    obj.flowLayout15:setMinScaledWidth(235);
     obj.flowLayout15:setHorzAlign("justify");
     obj.flowLayout15:setName("flowLayout15");
     obj.flowLayout15:setMargins({left=1, right=1, top=2, bottom=2});
